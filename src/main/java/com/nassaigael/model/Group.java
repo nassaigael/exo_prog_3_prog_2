639addbc5a20d94737b7224ee3745085c20c70cc
nassaigael <hei.nassai@gmail.com>
2025-11-13 20:00:01 +0300

    added classe group

---
+++ src/main/java/com/nassaigael/model/Group.java
@@ -0,0 +1,11 @@
+package com.nassaigael.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Group {
+    private final int id;
+    private final String name;
+}
