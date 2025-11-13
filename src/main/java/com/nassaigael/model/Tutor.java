339ba0776f95f8760373101db8cebe0aeebe5a5c
nassaigael <hei.nassai@gmail.com>
2025-11-13 20:01:21 +0300

    created and finished Tutor classe

--- src/main/java/com/nassaigael/model/Tutor.java
+++ src/main/java/com/nassaigael/model/Tutor.java
@@ -1,4 +1,16 @@
 package com.nassaigael.model;
 
-public class Tutor {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class Tutor extends User{
+    private final String description;
+
+    public Tutor(int id, String firstname, String lastname, LocalDate birthdate, String email, String phoneNumber, String description) {
+        super(id, firstname, lastname, birthdate, email, phoneNumber);
+        this.description = description;
+    }
 }
